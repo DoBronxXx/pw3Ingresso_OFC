98e29cc81054cbea5804e568bb66c01f3764dba3
Angelo Abreu <angelomello538@gmail.com>
2026-09-08 08:59:46 -0300

    Envio 08/09

---
+++ pw3-ingresso-ui-v2/src/app/pages/admin/sala/sala-form/sala-form.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ContainerComponent } from '../../../../shared/components/container/container';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-sala-form',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, ContainerComponent, RouterLink, ReactiveFormsModule],
+  templateUrl: './sala-form.html',
+  styleUrl: './sala-form.css'
+})
+export class SalaFormComponent {
+  private fb = inject(FormBuilder);
+
+  formSala = this.fb.group({
+    id: [0],
+    nome: [''],
+    preco: [0]
+  });
+
+  save(): void{
+    console.log(this.formSala.value);
+  }     
+}
